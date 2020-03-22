efcb6dffc648ff175777f71d510068ef65eb4d93
wwjjava <wwjjava>
2020-03-22 15:21:25 +0800

    123 commit

--- src/main/java/com/kaola/test/Student.java
+++ src/main/java/com/kaola/test/Student.java
@@ -2,7 +2,7 @@ package com.kaola.test;
 
 public class Student {
 	public static void main(String[] args) {
-		System.out.println("hello word!!!!  and java");
+		System.out.println("hello word!!!!  and java and 123");
 	}
 
 }
