07511e9231652e7fa1b1a5e68da4e86da4d2796e
wwj <z18310096192@163.com>
2020-08-22 20:15:56 +0800

    新增输出语句

--- src/main/java/com/kaola/test/Student.java
+++ src/main/java/com/kaola/test/Student.java
@@ -1,9 +1,13 @@
 package com.kaola.test;
 
+import java.util.Arrays;
+
 public class Student {
 	public static void main(String[] args) {
 		System.out.println("hello word!!!!  and java and 123");
 		System.out.println("hello word!!!!  and java and abc");
+		String [] str = {"1","2","3"};
+		System.out.println(Arrays.toString(str));
 	}
 
 }
